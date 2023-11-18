ac22a027693938d25f7ef4e36f3ac66eb556d86b
giovaniflp <giovanifeitosa123@gmail.com>
2023-11-18 15:34:35 -0300

    Azure speech service e .env adcionados, fazer ajustes necessários

---
+++ src/azurespeechtotext/speech.js
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+// This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
+speechConfig.speechRecognitionLanguage = "pt-BR", "en-US";
+
+//Botar o speech key da conta da azure e a região num arquivo .env
+
+function fromFile() {
+    let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("YourAudioFile.wav"));
+    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+    speechRecognizer.recognizeOnceAsync(result => {
+        switch (result.reason) {
+            case sdk.ResultReason.RecognizedSpeech:
+                console.log(`RECOGNIZED: Text=${result.text}`);
+                break;
+            case sdk.ResultReason.NoMatch:
+                console.log("NOMATCH: Speech could not be recognized.");
+                break;
+            case sdk.ResultReason.Canceled:
+                const cancellation = sdk.CancellationDetails.fromResult(result);
+                console.log(`CANCELED: Reason=${cancellation.reason}`);
+
+                if (cancellation.reason == sdk.CancellationReason.Error) {
+                    console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                    console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+                    console.log("CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+        speechRecognizer.close();
+    });
+}
+fromFile();
